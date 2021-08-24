5301ae039aedd573d12af977909816e1522623ea
Breno Menezes <brenoma@live.com>
2021-08-24 20:07:22 -0300

    Configuring server

---
+++ controllers/atentimendos.js
@@ -0,0 +1,11 @@
+module.exports = app => {
+  app.get('/atendimentos', (req, res) => {
+    res.send("Você está na rota de atendimentos realizando um GET.")
+  })
+
+  app.post('/atendimentos', (req, res) => {
+    console.log(req.body)
+
+    res.send("Você está na rota de atendimentos realizando um POST.")
+  })
+}
