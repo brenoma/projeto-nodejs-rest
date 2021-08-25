2114413ecf26e52c664f2da6bf28192ed23f5181
Breno Menezes <brenoma@live.com>
2021-08-24 21:21:27 -0300

    Date treatment

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,14 @@
+const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
   adiciona(atendimento) {
+    const dataCriacao = new moment().format('YYYY-MM-DD HH:MM:SS')
+    const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+    const atendimentoDatado = {...atendimento, dataCriacao, data}
     const sql = 'INSERT INTO Atendimentos SET ?'
 
-    conexao.query(sql, atendimento, (err, results) => {
+    conexao.query(sql, atendimentoDatado, (err, results) => {
       if (err) {
         console.log(err)
       } else {
